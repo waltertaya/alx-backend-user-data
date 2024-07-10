31e1258bc0712b2bc7924b204d784757c5ce5d09
waltertaya <waltermittybecker5@gmail.com>
2024-07-10 17:40:41 +0300

    feat: Implement session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 from flask_cors import (CORS, cross_origin)
 import os
 
@@ -19,8 +20,10 @@ auth = None
 
 if os.getenv('AUTH_TYPE') == 'basic_auth':
     auth = BasicAuth()
+elif os.getenv('AUTH_TYPE') == 'session_auth':
+    auth = SessionAuth()
 else:
-    auth = Auth()
+    auth = None
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module of Users views
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Session Auth class
+    """
+    pass
